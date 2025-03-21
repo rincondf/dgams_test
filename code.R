c5c1cfd9c286d939957580990eaf2dc2be6b2e01
Rincon <diego.rincon@wsu.edu>
2025-03-20 18:56:14 -0700

    added plots

--- code.R
+++ code.R
@@ -185,13 +185,11 @@ plot_predictions(mod,
 
 
 
-mod2 <- mvgam(counts ~
-                
-                # Hierarchical intercepts capture variation in average
-                s(series, bs = "re") +
-                # relative abundances
+mod2 <- mvgam(counts ~ 0,
+              
+              trend_formula = ~
                 s(DDs, k = 6, bs = 'fs') +
-                s(year, k = 4, bs = 'fs'),
+                s(year, k = 4, bs = 'fs') - 1,
               
               # Condition on the training data
               data = dat_train,
@@ -199,7 +197,7 @@ mod2 <- mvgam(counts ~
               # Automatically compute forecasts for the test data
               newdata = dat_test,
               
-              trend_model = "None",
+              trend_model = AR(3),
               
               # Beta observations with independent precisions
               family = Gamma(),
@@ -207,6 +205,7 @@ mod2 <- mvgam(counts ~
               # cmdstanr is highly recommended over rstan as 
               # it is much more up-to-date with the latest 
               # features in the Stan universe
+              noncentred = TRUE,
               backend = 'cmdstanr')
 summary(mod2)
 
@@ -330,17 +329,6 @@ plot_predictions(mod2,
                  condition = c('DDs', 'series'),
                  type = 'link',
                  conf_level = 0.5)
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -515,4 +503,575 @@ loo_compare(mod,
             mod3)
 
 
+
+
+
+
+
+
+
+
+mod4 <- mvgam(counts ~
+                
+                s(DDs, bs = "fs", k = 8) +
+                s(year, by = series, bs = "cr", k = 5),
+                
+                
+              
+              # Condition on the training data
+              data = dat_train,
+              
+              # Automatically compute forecasts for the test data
+              newdata = dat_test,
+              
+              trend_model = "None",
+              
+              # Beta observations with independent precisions
+              family = Gamma(),
+              
+              # cmdstanr is highly recommended over rstan as 
+              # it is much more up-to-date with the latest 
+              # features in the Stan universe
+              backend = 'cmdstanr')
+summary(mod4)
+
+gratia::draw(mod4)
+conditional_effects(mod4, points = 0.5)
+conditional_effects(mod4,
+                    type = 'link')
+
+
+marginaleffects::avg_predictions(mod4, variable = 'series')
+pp_check(mod4,
+         type = "ecdf_overlay_grouped",
+         group = "series",
+         ndraws = 50)
+
+
+
+hcs4 <- hindcast(mod4)
+
+plot(hcs4, series = 1)
+plot(hcs4, series = 2)
+plot(hcs4, series = 3)
+plot(hcs4, series = 4)
+plot(hcs4, series = 5)
+plot(hcs4, series = 6)
+plot(hcs4, series = 7)
+
+
+plot(mod4, type = 'forecast', series = 1)
+plot(mod4, type = 'forecast', series = 2)
+plot(mod4, type = 'forecast', series = 3)
+plot(mod4, type = 'forecast', series = 4)
+plot(mod4, type = 'forecast', series = 5)
+plot(mod4, type = 'forecast', series = 6)
+plot(mod4, type = 'forecast', series = 7)
+
+plot_predictions(mod4, by = "time", points = 0.5)
+plot_predictions(mod4, condition = c("time", "series", "series"), points = 0.5)
+
+
+
+plot_predictions(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_comparisons(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # At each time in 'year_seq', calculate contrasts between
+  # each pair of time series
+  variables = list(series = 'pairwise'),
+  
+  # Plot the difference trends using year as the x-axis
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_predictions(mod4,
+                 condition = c('year', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+
+
+plot_predictions(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(DDs = DD_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'DDs',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+
+plot_predictions(mod4,
+                 condition = c('DDs', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mod5 <- mvgam(counts ~ s(series, bs = "re"),
+              
+              # Hierarchical intercepts capture variation in average
+              
+              
+              trend_formula = ~
+                # relative abundances
+                s(DDs, k = 6, bs = 'fs') +
+                s(year, k = 4, bs = 'fs') - 1,
+              # Condition on the training data
+              data = dat_train,
+              
+              # Automatically compute forecasts for the test data
+              newdata = dat_test,
+              
+              trend_model = AR(1, cor = TRUE),
+              
+              # Beta observations with independent precisions
+              family = Gamma(),
+              
+              # cmdstanr is highly recommended over rstan as 
+              # it is much more up-to-date with the latest 
+              # features in the Stan universe
+              noncentred = TRUE,
+              backend = 'cmdstanr')
+summary(mod5)
+
+gratia::draw(mod5)
+conditional_effects(mod5, points = 0.5)
+conditional_effects(mod5,
+                    type = 'link')
+
+
+marginaleffects::avg_predictions(mod4, variable = 'series')
+pp_check(mod4,
+         type = "ecdf_overlay_grouped",
+         group = "series",
+         ndraws = 50)
+
+
+
+hcs4 <- hindcast(mod4)
+
+plot(hcs4, series = 1)
+plot(hcs4, series = 2)
+plot(hcs4, series = 3)
+plot(hcs4, series = 4)
+plot(hcs4, series = 5)
+plot(hcs4, series = 6)
+plot(hcs4, series = 7)
+
+
+plot(mod4, type = 'forecast', series = 1)
+plot(mod4, type = 'forecast', series = 2)
+plot(mod4, type = 'forecast', series = 3)
+plot(mod4, type = 'forecast', series = 4)
+plot(mod4, type = 'forecast', series = 5)
+plot(mod4, type = 'forecast', series = 6)
+plot(mod4, type = 'forecast', series = 7)
+
+plot_predictions(mod4, by = "time", points = 0.5)
+plot_predictions(mod4, condition = c("time", "series", "series"), points = 0.5)
+
+
+
+plot_predictions(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_comparisons(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # At each time in 'year_seq', calculate contrasts between
+  # each pair of time series
+  variables = list(series = 'pairwise'),
+  
+  # Plot the difference trends using year as the x-axis
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_predictions(mod4,
+                 condition = c('year', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+
+
+plot_predictions(
+  model = mod4, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(DDs = DD_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'DDs',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+
+plot_predictions(mod4,
+                 condition = c('DDs', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mod6 <- mvgam(counts ~ s(series, bs = "re"),
+               
+               # Hierarchical intercepts capture variation in average
+               
+              
+              trend_formula = ~
+                # relative abundances
+                s(DDs, k = 6, bs = 'fs') +
+                s(year, k = 4, bs = 'fs') - 1,
+             
+             # Condition on the training data
+             data = dat_train,
+             
+             # Automatically compute forecasts for the test data
+             newdata = dat_test,
+             
+             trend_model = AR(3),
+             
+             # Beta observations with independent precisions
+             family = Gamma(),
+             
+             # cmdstanr is highly recommended over rstan as 
+             # it is much more up-to-date with the latest 
+             # features in the Stan universe
+             noncentred = TRUE,
+             backend = 'cmdstanr')
+summary(mod6)
+
+
+conditional_effects(mod6, points = 0.5)
+conditional_effects(mod6,
+                    type = 'link')
+
+
+marginaleffects::avg_predictions(mod6, variable = 'series')
+pp_check(mod6,
+         type = "ecdf_overlay_grouped",
+         group = "series",
+         ndraws = 50)
+
+
+
+hcs6 <- hindcast(mod6)
+
+
+plot(hcs6, series = 1)
+plot(hcs6, series = 2)
+plot(hcs6, series = 3)
+plot(hcs6, series = 4)
+plot(hcs6, series = 5)
+plot(hcs6, series = 6)
+plot(hcs6, series = 7)
+
+
+
+plot(mod6, type = 'forecast', series = 1)
+plot(mod6, type = 'forecast', series = 2)
+plot(mod6, type = 'forecast', series = 3)
+plot(mod6, type = 'forecast', series = 4)
+plot(mod6, type = 'forecast', series = 5)
+plot(mod6, type = 'forecast', series = 6)
+plot(mod6, type = 'forecast', series = 7)
+
+plot_predictions(mod6, by = "time", points = 0.5)
+plot_predictions(mod6, condition = c("time", "series", "series"), points = 0.5)
+
+
+plot_predictions(
+  model = mod6, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_comparisons(
+  model = mod, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(year = year_seq,
+                     series = unique),
+  
+  # At each time in 'year_seq', calculate contrasts between
+  # each pair of time series
+  variables = list(series = 'pairwise'),
+  
+  # Plot the difference trends using year as the x-axis
+  by = 'year',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+plot_predictions(mod6,
+                 condition = c('year', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+DD_seq <- seq(min(dat_train$DDs), 
+              max(dat_train$DDs), 
+              length.out = 20)
+
+
+
+plot_predictions(
+  model = mod6, 
+  
+  # Predict over the sequence of times in 'year_seq'
+  newdata = datagrid(DDs = DD_seq,
+                     series = unique),
+  
+  # Use by = 'year' to ensure predictions are averaged
+  # over the sequence of times in year_seq
+  by = 'DDs',
+  
+  # Compute predictions on the link scale
+  type = 'link'
+)
+
+
+
+
+
+plot_predictions(mod6,
+                 condition = c('DDs', 'series'),
+                 type = 'link',
+                 conf_level = 0.5)
+
+
+loo_compare(mod,
+            mod2,
+            mod4,
+            mod5,
+            mod6)
+
+
+
+
+
+
+fc_mod <- forecast(mod)
+fc_mod2 <- forecast(mod2)
+fc_mod3 <- forecast(mod3)
+fc_mod4 <- forecast(mod4)
+fc_mod5 <- forecast(mod5)
+fc_mod6 <- forecast(mod6)
+
+
+
+
+sc_mod <- score(fc_mod)
+sc_mod2 <- score(fc_mod2)
+sc_mod3 <- score(fc_mod3)
+sc_mod4 <- score(fc_mod4)
+sc_mod5 <- score(fc_mod5)
+sc_mod6 <- score(fc_mod6)
+
+par(mar = c(5, 5, 2, 2))
+plot(sc_mod$all_series$eval_horizon, sc_mod$all_series$score, 
+     type = "o", lwd = 2, xlab = "Prediction horizon", ylab = "Error score", 
+     cex.lab = 1.8, cex.axis = 1.8, ylim = c(0, 150))
+abline(h = 0, lty = 2)
+
+
+
+par(mar = c(5, 5, 2, 2))
+plot(sc_mod6$all_series$eval_horizon, sc_mod6$all_series$score, 
+     type = "o", lwd = 2, xlab = "Prediction horizon", ylab = "Error score", 
+     cex.lab = 1.8, cex.axis = 1.8, ylim = c(0, 150))
+abline(h = 0, lty = 2)
+
+
+plot(sc_mod2$all_series$eval_horizon, sc_mod2$all_series$score)
+plot(sc_mod3$all_series$eval_horizon, sc_mod3$all_series$score)
+plot(sc_mod4$all_series$eval_horizon, sc_mod4$all_series$score)
+plot(sc_mod5$all_series$eval_horizon, sc_mod5$all_series$score)
+plot(sc_mod6$all_series$eval_horizon, sc_mod6$all_series$score)
+
+
+sum(sc_mod$all_series$score, na.rm = TRUE)
+sum(sc_mod2$all_series$score, na.rm = TRUE)
+sum(sc_mod3$all_series$score, na.rm = TRUE)
+sum(sc_mod4$all_series$score, na.rm = TRUE)
+sum(sc_mod5$all_series$score, na.rm = TRUE)
+sum(sc_mod6$all_series$score, na.rm = TRUE)
+
+
+ss <- "Basin City"
+aa <- "fc_mod"
+fc_mod$forecasts$paste(ss)
+
+paste0(aa, "$", "forecasts", "$", ss)
+
+
+plot_blh <- function(model, region) {
+  
+  
+  lowBC <- rep(NA, 31)
+  
+  for(i in 1: 31) {
+    lowBC[i] <- as.numeric(summary(model$forecasts[region][[1]][, i])[2])
+  }
+  
+  hiBC <- rep(NA, 31)
+  
+  for(i in 1: 31) {
+    hiBC[i] <- as.numeric(summary(model$forecasts[region][[1]][, i])[5])
+  }
+  
+  
+  maxBC <- rep(NA, 31)
+  
+  for(i in 1: 31) {
+    maxBC[i] <- as.numeric(quantile(model$forecasts[region][[1]][, i], probs = 0.95))
+  }
+  
+  minBC <- rep(NA, 31)
+  
+  for(i in 1: 31) {
+    minBC[i] <- as.numeric(quantile(model$forecasts[region][[1]][, i], probs = 0.05))
+  }
+  
+  
+  par(mar = c(5, 5, 2, 2))
+  plot(seq(361, 391), maxBC, xlim = c(360, 395), type = "l", lty = 3,
+       xlab = "Time (weeks)", ylab = "Counts", cex.lab = 1.8)
+  lines(seq(361, 391), lowBC, lty = 2)
+  points(dat_test$time[which(dat_test$series == 
+                              levels(dat_test$series)[region])], 
+        dat_test$counts[which(dat_test$series == 
+                                levels(dat_test$series)[region])],
+        lwd = 2)
+  lines(seq(361, 391), hiBC, lty = 2)
+  lines(seq(361, 391), minBC, lty = 3)
+  lines(seq(361, 391), colMeans(model$forecasts[region][[1]]), lwd = 2)
+  
+}
+
+plot_blh(fc_mod, region = 1)
+plot_blh(fc_mod, region = 2)
+plot_blh(fc_mod, region = 3)
+plot_blh(fc_mod, region = 4)
+plot_blh(fc_mod, region = 5)
+plot_blh(fc_mod, region = 6)
+plot_blh(fc_mod, region = 7)
+
+
+
+
+
+
+plot_blh(fc_mod6, region = 1)
+plot_blh(fc_mod6, region = 2)
+plot_blh(fc_mod6, region = 3)
+plot_blh(fc_mod6, region = 4)
+plot_blh(fc_mod6, region = 5)
+plot_blh(fc_mod6, region = 6)
+plot_blh(fc_mod6, region = 7)
 
